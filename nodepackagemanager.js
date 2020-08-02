caca270c163b1ba497f423c84741cff273a1c1fd
otumian <popecan1000@gmail.com>
2020-08-02 17:46:29 +0000

    nodepackagemanager.js

---
+++ nodepackagemanager.js
@@ -0,0 +1,19 @@
+// node packahe manger - npm
+// https://www.npmjs.com is where you can find more about nodejs packages
+// npm is a cli-tool for mananging nodejs packages/modules
+// npm install module - installs the module
+// npm uninstall module - uninstalls the module
+
+
+// package.json
+// package.json is a file one can use to keep track of all the packages
+// that the node app is using
+// this can be created manually or by a command
+// npm init - creates a package.json file
+
+// npm install package(s) -save - adds package to dependencies
+
+// npm install - will install packages from the package.json file
+
+// npm install -g package(s) or
+// npm install package(s) -g  - installs packages globally
